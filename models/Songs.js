02493230735a6baa7f2e4b02f3ae8efd1559d7b4
dorin131 <dorin.fodor@gmail.com>
2016-06-01 00:59:31 +0100

    Saving new songs to the db

--- models/Songs.js
+++ models/Songs.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var SongSchema = new mongoose.Schema({
   title: String,
+  airTime: String,
   timesPlayed: {type: Number, default: 1},
   lastTimePlayed: {type: Date, default: Date.now}
 });
--- modules/retrieveSongs.js
+++ modules/retrieveSongs.js
@@ -1,5 +1,7 @@
 module.exports.init = function(){
   var http = require('http');
+  var mongoose = require('mongoose');
+  var Song = mongoose.model('Song');
 
   var songArray = [];
 
@@ -27,18 +29,40 @@ module.exports.init = function(){
   separateSongs = function(songList) {
     songArray = songList.split(/\r\n|\r|\n/);
     for (i = 0; i < songArray.length - 1; i++) {
-      songArray[i] = songArray[i].replace('<li>', '').replace('</li>', '').substring(6);
-      console.log('Song ' + (i+1) + ': ' + songArray[i]);
-      saveSong(songArray[i]);
+      songArray[i] = songArray[i].replace('<li>', '').replace('</li>', '');
+      //console.log('Song ' + (i+1) + ': ' + songArray[i]);
+      songExists(songArray[i]);
     };
   };
 
-  saveSongs = function(song){
-    if (songExists(song)) {
-      incrementCounter(song);
-    } else {
-      addNewSong(song);
-    };
+  songExists = function(song) {
+    Song.findOne({title: new RegExp(song.substring(6), 'i')}, function(err, res) {
+      if (err) {
+        return console.log('Error', err);
+      }
+      if (res) {
+        incrementCounter(song);
+      } else {
+        addNewSong(song);
+      }
+    });
+  };
+
+  incrementCounter = function(song) {
+    console.log('Song exists. Incrementing.');
+  };
+
+  addNewSong = function(song) {
+    var song = new Song({
+      title: song.substring(6),
+      airTime: song.substring(0, 5)
+    });
+    song.save(function(err, res) {
+      if (err) {
+        return console.log('Error: ', err);
+      };
+      console.log('Song added to db');
+    });
   };
 
 };
