4eb587acd9bf18947f47b9c7ab2df0d62c685031
dorin131 <dorin.fodor@gmail.com>
2016-06-25 00:09:33 +0100

    Added link to download song list in CSV format

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Song = mongoose.model('Song');
 
+var json2csv = require('json2csv');
+var fields = ['id', 'title'];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -16,6 +19,19 @@ router.get('/songs', function(req, res, next) {
       return next(err);
     };
     res.json(songs);
+  });
+});
+
+/* Download list */
+router.get('/download', function(req, res, next) {
+  Song.find().lean().exec(function (err, file) {
+    json2csv({ data: file, fields: fields }, function(err, csv) {
+      if (err) console.log(err);
+      res.header('content-type','text/csv');
+      res.header('content-disposition', 'attachment; filename=songs.csv');
+      res.end(csv);
+      console.log(csv);
+    });
   });
 });
 
